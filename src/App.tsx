f29a4a7300c24080be3e5b0cfbc632eed0bc0890
NishChal370 <nishchal.370@gmail.com>
2022-09-07 19:36:31 +0545

    Setup project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Container } from '@mui/material';
+
+
+function App() {
+
+      return (
+            <Container className='App'>
+                  HEllo I am Jhola Pasal
+            </Container>
+      )
+}
+
+export default App
---
+++ src/theme/index.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+      typography: {
+            fontFamily: [
+                  'Sora', 'sans-serif',
+            ].join(','),
+      },
+});
+
+export default theme;
